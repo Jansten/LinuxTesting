1cc9256aed085a4e6800b16590932a07ee7966e7
Michael Hannon <bostonhannon@gmail.com>
2016-08-18 16:31:33 -0700

    Forgot to add comments

--- Python/if_number_guess.py
+++ Python/if_number_guess.py
@@ -1,4 +1,6 @@
-x = int(raw_input("What number am I thinking of between one and five? "))
+#Asks the user to guess a number between one and five, with the appropriate responses.
+
+x = int(raw_input("What number am I thinking of between 1 and 5? "))
 
 if x == 4:
     print('You guessed correctly!')
