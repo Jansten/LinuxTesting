10ec6745c5fff2cf46c072b78ee94ead3826be82
Michael Hannon <bostonhannon@gmail.com>
2016-08-18 16:33:10 -0700

    Forgot to add credit

--- Python/if_number_guess.py
+++ Python/if_number_guess.py
@@ -1,4 +1,5 @@
 #Asks the user to guess a number between one and five, with the appropriate responses.
+#Credit for the basic program to: http://pythonprogramminglanguage.com/if-statements
 
 x = int(raw_input("What number am I thinking of between 1 and 5? "))
 
