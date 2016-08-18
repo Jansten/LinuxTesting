6f2e7d9d6852fa2a1dabada42197913affb3db9a
Michael Hannon <bostonhannon@gmail.com>
2016-08-18 16:29:15 -0700

    A script to have you guess a number

---
+++ Python/if_number_guess.py
@@ -0,0 +1,8 @@
+x = int(raw_input("What number am I thinking of between one and five? "))
+
+if x == 4:
+    print('You guessed correctly!')
+else:
+    print('Wrong guess, try again!')
+
+print('End of program.')
