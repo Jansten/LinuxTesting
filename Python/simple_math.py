1941fc4e67df5e64ff912250bd71ba7ba898bb71
Michael Hannon <bostonhannon@gmail.com>
2016-08-18 17:26:54 -0700

    Added a few features
    For each section, the user can now input what number they’d like to use
    to use in the calculation.
    
    Additionally, each section should have clearer formatting and should
    change the text based on what the user inputs.

--- Python/simple_math.py
+++ Python/simple_math.py
@@ -1,25 +1,30 @@
-#simple_math.py 
+#simple_math.py v2
 #2016, Michael Hannon
 #Performs some simple math on a base number, with clear text output
+#v2 added user input to determine what numbers are used in various math forumlas (formulae?)
 
 #Prompt the user to enter a whole number (no decimals)
 base_num = int(raw_input("Please enter a whole number: "))
 
 #Print the number the user entered before doing any math
-print "Your base number is: {}".format(base_num)
+print "\nYour base number is: {}".format(base_num)
 
-#Add 100 to the base number
-add_num = base_num + 100
-print "Your base number + 100 is: {}".format(add_num)
+#Prompt the user to enter a number to use for addition
+add_by_num = int(raw_input("\nPlease enter a number to add to {}: ".format(base_num)))
+add_num = base_num + add_by_num
+print "Your base number plus {} is: {}".format(add_by_num,add_num)
 
-#Divide the base number by two
-sub_num = base_num / 2
-print "Your base number divided by two is: {}".format(sub_num)
+#Prompt the user to enter a number to use in division
+div_by_num = int(raw_input("\nPlease enter a number to divide {} by: ".format(base_num)))
+div_num = base_num / div_by_num
+print "Your base number divided by {} is: {}".format(div_by_num,div_num)
 
-#Multiply the base number by three
-mul_num = base_num * 3
-print "Your base number multiplied by three is: {}".format(mul_num)
+#Prompt the user to enter a number to use in multiplication
+mul_by_num = int(raw_input("\nPlease enter a number to multiply {} by: ".format(base_num)))
+mul_num = base_num * mul_by_num
+print "Your base number multiplied by {} is: {}".format(mul_by_num,mul_num)
 
-#Set the base number to the power of two
-pow_num = base_num ** 2
-print "Your base number to the power of two is: {}".format(pow_num)
+#Prompt the user to enter a number to use as an exponent
+pow_by_num = int(raw_input("\nPlease enter a number to use as an exponent for {}: ".format(base_num)))
+pow_num = base_num ** pow_by_num
+print "Your base number to the power of {} is: {}".format(pow_by_num,pow_num)
