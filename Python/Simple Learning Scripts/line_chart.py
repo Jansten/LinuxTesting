67822a8a991a7f4efae2a020f8d988bd93ac8b96
Michael Hannon <bostonhannon@gmail.com>
2016-08-19 20:32:53 -0700

    Simple line chart

---
+++ Python/Simple Learning Scripts/line_chart.py
@@ -0,0 +1,11 @@
+#Prints a simple line chart
+#Code cerdit goes to: http://pythonprogramminglanguage.com/line-charts
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [2,3,4,5,7,9,13,15,17]
+plt.plot(x)
+plt.ylabel('Sunlight')
+plt.xlabel('Time')
+plt.show()
