cc5e6c6baf980836440d9f182e4243e5a7a798ef
Michael Hannon <bostonhannon@gmail.com>
2016-08-19 21:16:12 -0700

    Initial checkin of pig_latin.py
    A short script that takes a user-entered string and converts it to Pig
    Latin.

---
+++ Python/Simple Learning Scripts/pig_latin.py
@@ -0,0 +1,17 @@
+# pig_latin.py
+# 2016-08-19, Michael Hannon
+# Takes a user entered word and "translates" it to Pig Latin
+# Based on code written for a CodeCademy course
+
+# Set a varliable for the "ay" portion of Pig Latin
+pyg = "ay"
+
+original = raw_input("Please enter some text: ")
+
+if len(original) > 0 and original.isalpha(): #Validate that the user has entered text that also does not contain numbers
+    word = original.lower() #Convert the string to lowercase
+    first = word[0]
+    new_word = word[1:len(word)] + first + pyg #Switch the string around, then add the Pig Latin part
+    print new_word
+else:
+    print "Error: Please enter a string without spaces." #Code cannot currently parse a string with spaces; future improvement.
