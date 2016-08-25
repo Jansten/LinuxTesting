be8cb6fecf4314cb75f90aa8de0d8be96bfa34ae
Michael Hannon <bostonhannon@gmail.com>
2016-08-24 18:35:56 -0700

    Final cleanup pass
    Cleaned up a few of the console outputs (the CodeCademy prompts weren’t
    the most friendly) and making some minor improvements on some
    formatting (because I’m particular like that).

--- Python/battleship_game.py
+++ Python/battleship_game.py
@@ -35,22 +35,22 @@ ship_col = random_col(board)
 # The main game.  Have the user input the row and column, then check whether it's the battleship or not.
 # If the user sinks the battleship, exit the game.  If not, allow four guesses.
 for turn in range(4):
-    guess_row = int(raw_input("Guess Row:"))
-    guess_col = int(raw_input("Guess Col:"))
+    guess_row = int(raw_input("Guess Row: "))
+    guess_col = int(raw_input("Guess Column :"))
 
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
         break
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print "Oops, that's not even in the ocean."
+            print "Oops, that's not on the board."
         elif(board[guess_row][guess_col] == "X"):
-            print "You guessed that one already."
+            print "You guessed that one already.  Try again!"
         else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "X"
         if turn == 3:
             print "Game Over"
         else:
-            print "Turn", turn + 1
+            print "Turn #", turn + 1
             print_board(board)
