6e930f42e64d05d5f9e75f76c67595d30f25ba22
Michael Hannon <bostonhannon@gmail.com>
2016-08-24 18:16:11 -0700

    New Version
    More error handling.  This time, for whether or not a user has guessed
    that location before.

--- Python/battleship_game.py
+++ Python/battleship_game.py
@@ -28,6 +28,8 @@ print ship_col
 # Write your code below!
 if guess_row == ship_row and guess_col == ship_col:
     print "Congratulations!  You sank my battleship!"
+elif board[guess_row][guess_col] == "X":
+    print "You guessed that one already."
 else:
     if guess_row not in range(5) or guess_col not in range(5):
         print "Oops, that's not even in the ocean."
