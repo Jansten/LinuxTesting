9976d0f3bf793ec714b48eeaacc82f753161c509
Michael Hannon <bostonhannon@gmail.com>
2016-08-24 17:54:45 -0700

    v6
    Adding user inputs for guessing which column and row the battleship may
    be in

--- Python/battleship_game.py
+++ Python/battleship_game.py
@@ -1,8 +1,8 @@
-from random import randint 
+from random import randint
 
 board = []
 
-for x in range(0, 5):
+for x in range(0,5):
     board.append(["O"] * 5)
 
 def print_board(board):
@@ -11,9 +11,13 @@ def print_board(board):
 
 def random_row(board):
     return randint(0, len(board) - 1)
-    
+
 def random_col(board):
-    return randint(0, len(board) - 1)
-    
-random_row(board)
-random_col(board)
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+# Add your code below!
+guess_row = int(raw_input("Guess Row: "))
+guess_col = int(raw_input("Guess Col: "))
