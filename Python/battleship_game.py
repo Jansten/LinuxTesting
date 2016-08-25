e122a8e98daafbd1f75ab66d4b7d75bf3702d513
Michael Hannon <bostonhannon@gmail.com>
2016-08-24 17:59:07 -0700

    v8
    Added functionality to check whether the user’s input matches the
    random numbers that have been generated.  Basically, wether or not the
    user sinks the battleship.

--- Python/battleship_game.py
+++ Python/battleship_game.py
@@ -2,12 +2,14 @@ from random import randint
 
 board = []
 
-for x in range(0,5):
+for x in range(0, 5):
     board.append(["O"] * 5)
 
 def print_board(board):
     for row in board:
         print " ".join(row)
+
+print_board(board)
 
 def random_row(board):
     return randint(0, len(board) - 1)
@@ -16,11 +18,13 @@ def random_col(board):
     return randint(0, len(board[0]) - 1)
 
 ship_row = random_row(board)
-print ship_row
-
 ship_col = random_col(board)
+guess_row = int(raw_input("Guess Row:"))
+guess_col = int(raw_input("Guess Col:"))
+
+print ship_row
 print ship_col
 
-# Add your code below!
-guess_row = int(raw_input("Guess Row: "))
-guess_col = int(raw_input("Guess Col: "))
+# Write your code below!
+if guess_row == ship_row and guess_col == ship_col:
+    print "Congratulations!  You sank my battleship!"
