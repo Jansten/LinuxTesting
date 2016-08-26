bd024969675ca59d765f01026907f3b3b59acec2
Michael Hannon <bostonhannon@gmail.com>
2016-08-26 12:09:35 -0700

    Initial check in

---
+++ Python/Simple Learning Scripts/is_prime.py
@@ -0,0 +1,14 @@
+# is_prime.py
+# 2016-06-28, Michael Hannon
+# Checks whether a number is a prime number or not
+# Written as part of a CodeCademy course
+
+def is_prime(x):
+    if x < 2:
+        return False
+    else:
+        for n in range(2, (x-1)):
+            if x % n == 0:
+                return False
+            else:
+                return True
