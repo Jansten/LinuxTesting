c65789fde9ecbea3f9d8aad675a7eec1a4a908f6
Michael Hannon <bostonhannon@gmail.com>
2016-09-09 13:38:58 -0700

    censor.py - Initial check in

---
+++ Python/Simple Learning Scripts/censor.py
@@ -0,0 +1,18 @@
+# censor.py
+# 2016-09-09, Michael Hannon
+# Take a string and censor a word in that string.
+# Written for a CodeCademy course
+# NOTE: I'm more than sure that this code could be cleaned up significantly
+
+def censor(text,word):
+    temp_text = text.split()
+    result = ""
+    for i in temp_text:
+        if i == str(word):
+            result += " " + ("*" * len(word))
+        else:
+            result += " " + i
+    clean_string = result[1:]
+    return clean_string
+    
+censor("this hack is wach hack", "hack")
