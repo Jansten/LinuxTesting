9c8113cb52733da1b5241f0a187d8f9b35ab3591
Michael Hannon <bostonhannon@gmail.com>
2018-02-19 20:14:52 -0800

    Added lesson materials: 02 - Using Comments

---
+++ Python/LinuxAcademy/Python 3 For System Administrators/02 - Using Comments/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.6
+
+# This is a program that prints "Hello World!"
+print("Hello World!")
+
+"""
+This isn't actually a block comment,
+it's really a multi-line string
+that doesn't get executed, but 
+takes up memory.
+"""
