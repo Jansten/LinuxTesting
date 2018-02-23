edd10d4c880e87a314364a7351938b44d2aebefa
Michael Hannon <bostonhannon@gmail.com>
2018-02-22 20:37:51 -0800

    Another set of LA files

---
+++ Python/LinuxAcademy/Python 3 For System Administrators/04 - Working With Variables/variables.py
@@ -0,0 +1,2 @@
+my_str = "This is a simple string"
+
---
+++ Python/LinuxAcademy/Python 3 For System Administrators/05 - Exercise - Creating and Displaying Variables/01 - create_and_display_variables.py
@@ -0,0 +1,7 @@
+first_name = "Michael"
+last_name = "Hannon"
+age = 37
+birth_date = "August 31st 1981"
+
+print(f"My name is {first_name} {last_name}")
+print(f"I was born on {birth_date}, and I'm {age} years old.")
