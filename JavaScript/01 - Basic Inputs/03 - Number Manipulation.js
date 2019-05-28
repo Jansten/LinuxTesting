06b1327d0c759180106b894756e68cca1aadca9f
Michael Hannon <bostonhannon@gmail.com>
2019-05-27 18:53:57 -0700

    Create 03 - Number Manipulation.js

---
+++ JavaScript/01 - Basic Inputs/03 - Number Manipulation.js
@@ -0,0 +1,4 @@
+console.log(Math.random() * 100) //Generate random number, multiply by 100
+console.log(Math.floor(Math.random() * 100)) //Generate a whole number, multiply by 100
+console.log(Math.ceil(43.8)) //Rounds up the provided number
+console.log(Number.isInteger(2017)) //Checks to see if the provided number is an integer
