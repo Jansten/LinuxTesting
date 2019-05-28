5e4f7522a69878870e29f4f90e5b98c33e201fc1
Michael Hannon <bostonhannon@gmail.com>
2019-05-27 18:58:33 -0700

    Create 01 - Old Style of Variable Declaration.js

---
+++ JavaScript/02 - Variables/01 - Old Style of Variable Declaration.js
@@ -0,0 +1,4 @@
+var favoriteFood = 'pizza' //Create and assign a variable using the old style of declaration
+var numOfSlices = 8
+console.log(favoriteFood) //Print variable
+console.log(numOfSlices) //Print other variable
