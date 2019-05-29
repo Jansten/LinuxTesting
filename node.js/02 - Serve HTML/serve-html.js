0939a92ddb0e47acc01221fc5582149de7fea595
Michael Hannon <bostonhannon@gmail.com>
2019-05-29 16:37:19 -0700

    Serve Up a Simple HTML Page

---
+++ node.js/02 - Serve HTML/serve-html.js
@@ -0,0 +1,17 @@
+// Source: https://www.nodejsera.com/nodejs-tutorial-day1-thebeginning.html
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+	console.log("Port Number : 3000");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+	fs.readFile('index.html', (err, data) => {
+		//checking for errors
+		if (err) 
+			throw err;
+		console.log("Operation Success");
+		//sending the response
+		res.end(data);
+	});
+}).listen(3000);
